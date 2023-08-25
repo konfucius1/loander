6076299cf69413f743789761ee4e86bebeef99b4
Thanakon <kimsanthanakon@gmail.com>
2023-08-25 18:56:16 +1000

    added flowbite react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from '@/components/ui/button'
 import { TabsDemo } from '@/pages/Home'
+import MyPage from './pages/MyPage'
 
 export default function App() {
   return (
     <div>
+      <MyPage />
       <Button>Click me</Button>
       <TabsDemo />
     </div>
