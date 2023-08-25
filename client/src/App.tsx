caf8230d5dd83c65952cad5fb428bb546a864972
Thanakon <kimsanthanakon@gmail.com>
2023-08-25 18:17:16 +1000

    set up vite react/typescript tailwindcss

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+import { TabsDemo } from '@/pages/Home'
+
+export default function App() {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <TabsDemo />
+    </div>
+  )
+}
