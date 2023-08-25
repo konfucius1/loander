879308befbda511d2e2fe45d35af20a16a091ca7
Thanakon <kimsanthanakon@gmail.com>
2023-08-26 00:36:14 +1000

    feat: add state management for filter search and reset with zustand store

--- client/src/components/browse/SliderFilter.tsx
+++ client/src/components/browse/SliderFilter.tsx
@@ -1,19 +1,52 @@
-import { cn } from '@/lib/utils'
 import { Slider } from '@/components/ui/slider'
+import {
+  SliderState,
+  SliderValues,
+  useSliderStore,
+} from '@/store/useSliderStore'
 
 type SliderProps = React.ComponentProps<typeof Slider>
 
 interface SliderFilterProps extends SliderProps {
-  label?: string
+  label: string
+  min: number
+  max: number
 }
 
-export function SliderFilter({ label, ...props }: SliderFilterProps) {
+const toCamelCase = (str: string) => {
+  return str.replace(/(?:^\w|[A-Z]|\b\w|\s+)/g, (match, index) => {
+    if (+match === 0) return ''
+    return index === 0 ? match.toLowerCase() : match.toUpperCase()
+  })
+}
+
+const getSliderValue = (
+  state: SliderValues,
+  key: keyof SliderValues
+): number => {
+  return state[key]
+}
+
+export function SliderFilter({ label, min, max, step }: SliderFilterProps) {
+  const sliderName = toCamelCase(label)
+
+  const sliderValue = useSliderStore((state) =>
+    getSliderValue(state, sliderName as keyof SliderState)
+  )
+
+  const setSliderValue = useSliderStore((state) => state.setSliderValue)
+
   return (
-    <>
-      <div className="sm:w-[20%] sm:mr-16 my-4">
-        {label && <label className="block mb-2">{label}</label>}
-        <Slider defaultValue={[50]} max={100} step={1} {...props} />
-      </div>
-    </>
+    <div className="sm:w-[20%] sm:mr-16 my-4">
+      {label && <label className="block mb-2">{label}</label>}
+      <Slider
+        value={[sliderValue]}
+        // defaultValue={[]}
+        min={min}
+        max={max}
+        step={step}
+        onValueChange={(values) => setSliderValue(sliderName, values[0])}
+      />
+    </div>
   )
 }
