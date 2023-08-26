75ddbbdb28abdfd5604751b07ebcf0455b10f0c2
yoootz <haroldforuni@gmail.com>
2023-08-26 17:36:28 +1000

    feat (Support): remove link to flowbite

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -30,11 +30,8 @@ export default function Footer() {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © 2023{' '}
-          <a href="https://flowbite.com/" className="hover:underline">
-            loander™
-          </a>
-          . All Rights Reserved.
+          © 2023 <a className="hover:underline">loander™</a>. All Rights
+          Reserved.
         </span>
       </div>
     </footer>
