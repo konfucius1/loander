95290eb0f8aff97fdfb1c187a29cb63ebfd53948
yoootz <haroldforuni@gmail.com>
2023-08-27 14:32:02 +1000

    feat (Matches): user cards and text box appear when clicked added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,25 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Browse } from './pages/Browse'
-import { Home } from './pages/Home'
-import { NavBar } from './components/Navbar'
-import { Support } from './pages/Support'
-import Footer from './components/footer'
-import { useEffect, useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Browse } from './pages/Browse';
+import { Home } from './pages/Home';
+import { NavBar } from './components/Navbar';
+import { Support } from './pages/Support';
+import Footer from './components/footer';
+import { useEffect, useState } from 'react';
+import { Matches } from './pages/Matches';
 
 export default function App() {
-  const [token, setToken] = useState(false)
+  const [token, setToken] = useState(false);
 
   if (token) {
-    sessionStorage.setItem('token', JSON.stringify(token))
+    sessionStorage.setItem('token', JSON.stringify(token));
   }
 
   useEffect(() => {
     if (sessionStorage.getItem('token')) {
-      const data = JSON.parse(sessionStorage.getItem('token'))
-      setToken(data)
+      const data = JSON.parse(sessionStorage.getItem('token'));
+      setToken(data);
     }
-  }, [])
+  }, []);
 
   return (
     <div className="">
@@ -27,12 +28,12 @@ export default function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/browse" element={<Browse />} />
-          <Route path="/messages" element={<div>Messages</div>} />
+          <Route path="/messages" element={<Matches />} />
           <Route path="/dashboard" element={<div>Dashboard</div>} />
           <Route path="/support" element={<Support />} />
         </Routes>
       </BrowserRouter>
       <Footer />
     </div>
-  )
+  );
 }
---
+++ client/src/pages/Matches.tsx
@@ -0,0 +1,9 @@
+import { MatchesProfile } from '@/components/matches/MatchesProfile';
+
+export const Matches = () => {
+  return (
+    <div className="p-8">
+      <MatchesProfile />
+    </div>
+  );
+};
